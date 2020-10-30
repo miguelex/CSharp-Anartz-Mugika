f222c842269ffccaf2b87f5155896cea37f225e5
Miguel Angel Delgado <miguelexepg@gmail.com>
2020-10-30 19:53:52 +0100

    Commit inicial
    Commit de apertura del repositorio

---
+++ Practica/2-manejo-mensajes-pantalla/EjercicioFinal/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EjercicioFinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nombre: ");
+            String nombre = Console.ReadLine();
+            Console.WriteLine("Apellidos: ");
+            String apellidos = Console.ReadLine();
+            Console.WriteLine("Edad: ");
+            String edad = Console.ReadLine();
+            Console.WriteLine("Afición: ");
+            String aficion = Console.ReadLine();
+
+            Console.WriteLine("=== INFORMACICIÓN BÁSICA ===");
+            Console.WriteLine($"Eres {nombre} {apellidos}.");
+            Console.WriteLine($"Tienes {edad} años.");
+            Console.WriteLine($"Hobby: {aficion}.");
+            Console.WriteLine("============================");
+        }
+    }
+}
---
+++ Teoria/2-manejo-mensajes-pantalla/MensajePant/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MensajePant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /**
+            // Trabajando con la funcion Write
+            Console.Write("Hello World! ");
+            Console.Write("Hello World!");
+            Console.Write("Hello World!");
+            Console.Write("Hello World!");
+            Console.Write("Hello World!");
+            // Trabajando con la funcion WriteLine
+            Console.WriteLine("Comenzamos con el apartado WriteLine");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("Ultimo");
+            **/
+            // Trabajando con la funcion ReadLine
+            Console.WriteLine("Introduce tu edad: ");
+            Console.WriteLine($"Mi edad es: {Console.ReadLine()}");
+            
+            
+        }
+    }
+}
