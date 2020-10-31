0c540c0badbfc687a7a7d476d4ec338b82caf160
Miguel Angel Delgado <miguelexepg@gmail.com>
2020-10-31 18:55:58 +0100

    Tipo de datos enteros

---
+++ Teoria/3-variables-constantes-type-variables/TypeDataVariablesExample/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace TypeDataVariablesExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tipos de datos númericos - Enteros - Con signo");
+
+            // sbyte
+            //sbyte sbyteInvalid = 130; <--- ERROR... seria un short
+            sbyte sbyteValid = 120;
+            Console.WriteLine(sbyteValid);
+
+            //short
+            //short shortValue = 32900; <--- ERROR... seria un int
+            short shortValue = 32368;
+            Console.WriteLine(shortValue);
+
+            //int
+            //int intValue = 329000000000; <--- ERROR... seria un Long
+            int intValue = 3560000;
+            Console.WriteLine(intValue);
+
+            //long
+            //long longValue = 329000000000000000000; <--- ERROR
+            long longValue = 3560000000000000000;
+            Console.WriteLine(longValue);
+
+             Console.WriteLine("Tipos de datos númericos - Enteros - Sin signo");
+
+            // byte
+            //byte byteInvalid = 256; <--- ERROR... seria un ushort
+            byte byteValid = 255;
+            Console.WriteLine(byteValid);
+
+            //ushort
+            //ushort shortValue = 66369; <--- ERROR... seria un uint
+            ushort ushortValue = 65535;
+            Console.WriteLine(ushortValue);
+
+            //uint
+            //uint intValue = 4698963258; <--- ERROR... seria un uLong
+            uint uintValue = 4294967295;
+            Console.WriteLine(uintValue);
+
+            //ulong
+            //ulong longValue = 9896326987521478962; <--- ERROR
+            ulong ulongValue = 18446744073709551615;
+            Console.WriteLine(ulongValue);
+
+        }
+    }
+}
