b9be72690ade8774a9a43dcd05e993a5928f7435
Miguel Angel Delgado <miguelexepg@gmail.com>
2020-11-01 11:50:09 +0100

    Conversion implicita

---
+++ Teoria/3-variables-constantes-type-variables/TypeCasting/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TypeCasting
+{
+    class Program
+    {
+        // char -> int -> long -> float -> double
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Tipo de conversión implicito");
+            
+            char character = 'a';
+            int characterInt = character;
+
+            Console.WriteLine($"Estamos conviertiendo el caracter '{character}' a un valor entero: {characterInt}"); 
+
+            character = 'm';
+            characterInt = character;
+
+            Console.WriteLine($"Estamos conviertiendo el caracter '{character}' a un valor entero: {characterInt}");
+
+            // int -> double / int -> float
+
+            int numberInt = 99;
+            double numberDouble = numberInt;
+            float numberFloat = numberInt;
+
+            Console.WriteLine($"Numero entero {numberInt} => float {numberFloat} => double {numberDouble}");
+
+            
+        }
+    }
+}
