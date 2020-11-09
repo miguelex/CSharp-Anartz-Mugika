61d1dea020aa56d305d30d2485b9b4345872e155
Miguel Angel Delgado <miguelexepg@gmail.com>
2020-11-09 19:05:59 +0100

    Sumas

---
+++ Teoria/3-variables-constantes-type-variables/OperatorArimetic/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OperatorArimetic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SUMA (+)");
+            Console.WriteLine($"5 +4 + 56 + 34 = {5 +4 + 56 + 34}");
+            Console.WriteLine($"5.34d + 4.34d + 56 + 34 = {5.34d + 4.34d + 56 + 34}");
+            Console.WriteLine($"(-25) +4 + (-56) + 34 = {(-25) +4 + (-56) + 34}");
+        }
+    }
+}
