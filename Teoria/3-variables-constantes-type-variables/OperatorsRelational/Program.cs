285680e0cef9a6258109cb3f3095fc94d6c1e82e
Miguel Angel Delgado <miguelexepg@gmail.com>
2020-11-30 19:55:57 +0100

    operadores relacionales

---
+++ Teoria/3-variables-constantes-type-variables/OperatorsRelational/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace OperatorsRelational
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Operadores relacionales");
+            int x = 78, y = 67, z = 78;
+            // Igualdad
+            bool result = x == y; // falso
+            result = x == z; // true
+            // Desigualdad
+            result = x != y; // true
+            result = x != z; // false
+            // Mayor que 
+            result = x > y; // true
+            result = x > z; // false
+            // Menor que 
+            result = x < y; // false
+            result = x < z; // false
+            // Mayor o igual que 
+            result = x >= y; // true
+            result = x >= z; // true
+            // Menor que 
+            result = x <= y; // false
+            result = x <= z; // true
+
+
+
+        }
+    }
+}
