b095a8243fac9a46ea5e0d6306cd8e69250941ba
Miguel Angel Delgado <miguelexepg@gmail.com>
2020-12-05 12:42:24 +0100

    ej1-Cuadrado

---
+++ Practica/3-Tipos-datos-basicos-operadores/Cuadrado/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Cuadrado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca un numero para calcualr su cuadrado");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"El resultado del cuadrado de {x} es {x*x}");
+        }
+    }
+}
