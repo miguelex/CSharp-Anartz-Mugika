0be271a73c79a949af63de271ee5d653fc6c30fe
Miguel Angel Delgado <miguelexepg@gmail.com>
2020-12-06 11:38:29 +0100

    ej2-Multiplicar tres numeros

---
+++ Practica/3-Tipos-datos-basicos-operadores/multiplicaTresNumeros/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace multiplicaTresNumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+
+            Console.WriteLine("INTRODUZCA EL PRIMER VALOR ENTERO");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("INTRODUZCA EL SEGUNDO VALOR ENTERO");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("INTRODUZCA EL TERCER VALOR ENTERO");
+            num3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"El resultado de la multiplicacion de {num1} * {num2} * {num3} es: {num1*num2*num3} ");
+        }
+    }
+}
