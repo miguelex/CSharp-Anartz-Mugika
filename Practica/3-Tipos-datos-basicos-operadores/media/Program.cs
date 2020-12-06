88cf1f76be3ec07643faa5f0cf7e03b3d6d2f303
Miguel Angel Delgado <miguelexepg@gmail.com>
2020-12-06 12:16:51 +0100

    ej4-media

---
+++ Practica/3-Tipos-datos-basicos-operadores/media/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace media
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1, nota2, nota3, nota4;
+
+            Console.WriteLine("Introduzca la nota de Matematicas I:");
+            nota1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Introduzca la nota de AMC:");
+            nota2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Introduzca la nota de Desarrollo de Interfaces:");
+            nota3 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Introduzca la nota de Elaboración de Proyectos Informáticos:");
+            nota4 = Convert.ToDouble(Console.ReadLine());
+            double media = (nota1 + nota2 + nota3 + nota4) / 4;
+            Console.WriteLine($"La nota media obtenida de las notas del curso es: {media}");
+        }
+    }
+}
