7b24d5e6a859d868be4a82963defa1bbaf932e9c
Miguel Angel Delgado <miguelexepg@gmail.com>
2020-12-07 11:35:41 +0100

    ej8-cociente

---
+++ Practica/3-Tipos-datos-basicos-operadores/cociente/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace cociente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1, numero2;
+
+            Console.WriteLine("Introduzca el dividendo");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Introduzca el divisor");
+            numero2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Conciente: {numero1 / numero2} / Resto: {numero1 % numero2}");
+
+        }
+    }
+}
