3584ed2fd447ee0d4c8f9b7649064ff49563d295
Miguel Angel Delgado <miguelexepg@gmail.com>
2020-12-07 11:39:02 +0100

    ej9-multiplo de 2

---
+++ Practica/3-Tipos-datos-basicos-operadores/multiplo2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace multiplo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1;
+
+            Console.WriteLine("Introduzca el valor a evaluar");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Es multiplo de 2 el valor {numero1}?: {numero1 % 2 == 0}");
+        }
+    }
+}
