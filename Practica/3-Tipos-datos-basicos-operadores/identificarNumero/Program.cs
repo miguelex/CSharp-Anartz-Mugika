580c7d200a78bce34a5ba2c5d1da6463ea0891cb
Miguel Angel Delgado <miguelexepg@gmail.com>
2020-12-07 11:52:58 +0100

    ej11-identificar numero

---
+++ Practica/3-Tipos-datos-basicos-operadores/identificarNumero/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace identificarNumero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1;
+            char numeroChar;
+
+            Console.WriteLine("Introduzca un caracter para mostrar su digito");
+            numeroChar = Convert.ToChar(Console.ReadLine());
+            numero1 = numeroChar;
+            Console.WriteLine($"Es numero {numeroChar} corresponde al char {numero1}");
+        }
+    }
+}
