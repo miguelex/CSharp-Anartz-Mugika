07a507389edb932b533a5a8f50530888dffedcd3
Miguel Angel Delgado <miguelexepg@gmail.com>
2020-12-07 11:17:24 +0100

    ej5-iguales

---
+++ Practica/3-Tipos-datos-basicos-operadores/iguales/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace iguales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1, numero2;
+
+            Console.WriteLine("Introduce el primer numero");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Introduce el segundo numero");
+            numero2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"El valor {numero1} es igual al valor {numero2}? {numero1 == numero2}");
+        }
+    }
+}
