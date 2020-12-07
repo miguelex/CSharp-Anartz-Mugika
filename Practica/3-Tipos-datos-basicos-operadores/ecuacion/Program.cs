a2801da875c4dc46169c474046a5e7fe31e87941
Miguel Angel Delgado <miguelexepg@gmail.com>
2020-12-07 13:16:11 +0100

    ej15-esfera

---
+++ Practica/3-Tipos-datos-basicos-operadores/ecuacion/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ecuacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int incognita, resultado;
+
+            Console.WriteLine("Introduce el valor de la incognita para realziar la operacion x^2 + 2x +1:");
+            incognita = Convert.ToInt32(Console.ReadLine());
+            resultado = (incognita * incognita) + (2 * incognita) + 1;
+            Console.WriteLine($"El resultado es: {resultado}");
+            Console.WriteLine($"¿El resultado {resultado} está dentro del rango -5, 5?: {resultado >= -5 && resultado <= 5}");
+
+        }
+    }
+}
---
+++ Practica/3-Tipos-datos-basicos-operadores/esfera/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace esfera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double PI = 3.14159265358979323846;
+
+            double superficie, volumen, radio;
+
+            Console.WriteLine(" Introduzca el radio en metros: ");
+            radio = Convert.ToDouble(Console.ReadLine());
+
+            superficie = 4 * PI * (radio * radio);
+            volumen = (4/3) * PI * (radio * radio * radio);
+
+            Console.WriteLine($"La superficie de la esfera es: {superficie} metros cuadrado");
+            Console.WriteLine($"El volumen de la esfera es: {volumen} metros cubicos");
+            {
+                
+            }
+
+        }
+    }
+}
