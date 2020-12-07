576e06ce123ff352c6f9aea1c33b5c95538c990c
Miguel Angel Delgado <miguelexepg@gmail.com>
2020-12-07 12:03:05 +0100

    ej12-operar

---
+++ Practica/3-Tipos-datos-basicos-operadores/operar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace operar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1, numero2;
+
+            Console.WriteLine("Introduce el primer numero:");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Introduce el segundo numero:");
+            numero2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"{numero1} + {numero2} = {numero1 + numero2}");
+            Console.WriteLine($"{numero1} - {numero2} = {numero1 - numero2}");
+            Console.WriteLine($"{numero1} * {numero2} = {numero1 * numero2}");
+            Console.WriteLine($"{numero1} / {numero2} = {numero1 / (double)numero2}");
+            Console.WriteLine($"{numero1} % {numero2} = {numero1 & numero2}");
+        }
+    }
+}
