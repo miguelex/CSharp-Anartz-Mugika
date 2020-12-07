c11e8643832e2515182c819b7d069169a59bf295
Miguel Angel Delgado <miguelexepg@gmail.com>
2020-12-07 11:29:33 +0100

    ej7- par o impar

---
+++ Practica/3-Tipos-datos-basicos-operadores/parImpar/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace parImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1, numero2;
+
+            Console.WriteLine("Introduce el primer numero");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Introduce el segundo numero");
+            numero2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Los dos son PARES o IMPARES {(numero1 % 2 == 0 && numero2 % 2 == 0) || (numero1 % 2 != 0 && numero2 % 2 != 0)}");
+        }
+    }
+}
