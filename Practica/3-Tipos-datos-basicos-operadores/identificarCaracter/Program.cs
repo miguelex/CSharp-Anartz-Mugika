0d232c42ff271a6174b3d9fa1186ada0e5c37d1c
Miguel Angel Delgado <miguelexepg@gmail.com>
2020-12-07 11:48:20 +0100

    ej10-Identificar Caracter

---
+++ Practica/3-Tipos-datos-basicos-operadores/identificarCaracter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace identificarCaracter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1;
+            char numeroChar;
+
+            Console.WriteLine("Introduzca un numero para mostrar su caracter");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+            numeroChar = (char)numero1;
+            Console.WriteLine($"Es numero {numero1} corresponde al char {numeroChar}");
+        }
+    }
+}
