ab6b9d47ff7da15a12237c392a1348b887eb42c0
Miguel Angel Delgado <miguelexepg@gmail.com>
2020-12-07 11:21:30 +0100

    ej6-mayor o igual

---
+++ Practica/3-Tipos-datos-basicos-operadores/mayorIgual/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace mayorIgual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1, numero2;
+
+            Console.WriteLine("Introduce el primer numero");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Introduce el segundo numero");
+            numero2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"El valor {numero1} es igual al valor {numero2}? {numero1 == numero2}");
+            Console.WriteLine($"El valor {numero1} es mayor o igual que el valor {numero2}? {numero1 >= numero2}");
+        }
+    }
+}
