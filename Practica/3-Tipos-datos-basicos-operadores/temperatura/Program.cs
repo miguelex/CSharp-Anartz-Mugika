9d58e8a19ef81477ae80f42e9cdc8fa1eac6b7bb
Miguel Angel Delgado <miguelexepg@gmail.com>
2020-12-07 13:24:51 +0100

    ej16-temperatura

---
+++ Practica/3-Tipos-datos-basicos-operadores/temperatura/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace temperatura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Celsius, Fahrenheit, Kelvin;
+
+            Console.WriteLine("Introduzca la cantidad de grados Celsisu (Cº) que quieres convertir a Fahrenheit y Kelvin: ");
+            Celsius = Convert.ToDouble(Console.ReadLine());
+
+            Fahrenheit = (Celsius * 1.8) + 32;
+            Kelvin = Celsius + 273;
+
+            Console.WriteLine($"Celsius: {Celsius}ºC =====> Fahrenheit: {Fahrenheit}ºF");
+            Console.WriteLine($"Celsius: {Celsius}ºC =====> Kelvin: {Kelvin}ºK");
+        }
+    }
+}
