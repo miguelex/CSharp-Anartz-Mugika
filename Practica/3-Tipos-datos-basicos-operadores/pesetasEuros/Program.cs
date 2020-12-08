0343a4d2fc6e46c36e9f9182c0c0ea0ea9426e60
Miguel Angel Delgado <miguelexepg@gmail.com>
2020-12-08 11:28:52 +0100

    ej20-pesetas a euros

---
+++ Practica/3-Tipos-datos-basicos-operadores/pesetasEuros/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace pesetasEuros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double EURO = 166.386;
+            double pesetasConvertir;
+
+            Console.WriteLine("Introduzca la cantidad de pesetas que quiere convertir:");
+            pesetasConvertir = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"La cantidad de de {pesetasConvertir} pesetas equivale a {pesetasConvertir / EURO} eur. ");
+        }
+    }
+}
