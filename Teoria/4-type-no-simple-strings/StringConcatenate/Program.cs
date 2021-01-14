a897f2923bc3ef29213ddb749c2cf0cb3ffd1d48
Miguel Angel Delgado <miguelexepg@gmail.com>
2021-01-14 11:35:48 +0100

    String - Concatenación - Operador suma

---
+++ Teoria/4-type-no-simple-strings/StringConcatenate/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace StringConcatenate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Concatenación - Operador sumar");
+
+            int x = 10, y = 29;
+            string helloMessage = "Hola!";
+            string result = "";
+
+            result = Convert.ToString(x + y); // "39" == OBJ : "1029"
+            result = Convert.ToString(x) + y; //"1029"
+            result = helloMessage + " " + x; // "Hola! 10"
+            result = x + "" + y; //"1029"
+
+
+            Console.WriteLine("Concatenación - Funcion Concat");
+            Console.WriteLine("Concatenación - Funcion Format");
+            Console.WriteLine("Concatenación - String Interpolation");
+        }
+    }
+}
