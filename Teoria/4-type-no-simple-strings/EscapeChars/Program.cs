dabbe7457a4717ed72b55ffbd19170e4533e6fc5
Miguel Angel Delgado <miguelexepg@gmail.com>
2021-01-14 13:04:04 +0100

    Caracteres especiales

---
+++ Teoria/4-type-no-simple-strings/EscapeChars/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace EscapeChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result="";
+            Console.WriteLine("Nueva línea");
+            result = "Migue\nDel\nMar";
+            Console.WriteLine(result);
+
+            Console.WriteLine("Tabulador");
+            result = "Migue\tDel\tMar";
+            Console.WriteLine(result);
+
+            Console.WriteLine("Contrabarra");
+            result = "Migue\\Del\\Mar";
+            Console.WriteLine(result);
+
+            Console.WriteLine("Comillas dobles y simpes");
+            result = "\"Migue\" 'Del' Mar";
+            Console.WriteLine(result);
+
+            Console.WriteLine("retorno de carro");
+            result = "Migue\rDel Mar";
+            Console.WriteLine(result);
+
+        }
+    }
+}
