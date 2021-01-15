367579e3eecfd3a2fc9ea4aed02689190277fea5
Miguel Angel Delgado <miguelexepg@gmail.com>
2021-01-15 11:09:40 +0100

    ToUpper / ToLower

---
+++ Teoria/4-type-no-simple-strings/ToUpperLower/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ToUpperLower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textOne = "aaaaaaaeeeeeEEEE";
+            string textTwo = "AAJKAAAAAAfifififififjsk";
+            string textThree = "AAUSSOSSPPSSMJDJKD";
+            string result ="";
+
+            Console.WriteLine("Convertir a mayusculas");
+            result = textOne.ToUpper();
+            result = textTwo.ToUpper();
+            result = textThree.ToUpper();
+
+            Console.WriteLine("Convertir a minusculas");
+            result = textOne.ToLower();
+            result = textTwo.ToLower();
+            result = textThree.ToLower();
+        }
+    }
+}
