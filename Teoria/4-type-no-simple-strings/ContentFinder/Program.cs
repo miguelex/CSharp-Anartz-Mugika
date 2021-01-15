8645e5f06074004b7a95c94b4f57a9a86d6c2c3c
Miguel Angel Delgado <miguelexepg@gmail.com>
2021-01-15 11:57:11 +0100

    StartsWith / EndsWith / Equals

---
+++ Teoria/4-type-no-simple-strings/ContentFinder/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ContentFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Migue";
+            bool result = false;
+
+            Console.WriteLine("Equals");
+            result = name.Equals("Migue"); // true
+            result = name.Equals("MiguE"); // false
+            result = name.Equals("migue"); // false
+
+            Console.WriteLine("Comienza con");
+            result = name.StartsWith("Migue"); // true
+            result = name.StartsWith("M"); // true
+            result = name.StartsWith("mi"); // false
+
+            Console.WriteLine("Termina con");
+            result = name.EndsWith("Migue"); // true
+            result = name.EndsWith("ue"); // true
+            result = name.EndsWith("E"); // false
+
+        }
+    }
+}
