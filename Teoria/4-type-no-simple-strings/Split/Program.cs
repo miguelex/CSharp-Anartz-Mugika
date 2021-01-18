cfef6e28e83fe6bd20918c5e4630f731ac48faf3
Miguel Angel Delgado <miguelexepg@gmail.com>
2021-01-18 11:32:39 +0100

    split

---
+++ Teoria/4-type-no-simple-strings/Split/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Split
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterVideoGames = "Mario/Luigi/Sack Boy/Lara Croft/SolidSnake/Sonic";
+
+            string [] characterList = characterVideoGames.Split("/");
+
+            string message = "¡Hola Anartz! ¿Que tal estas? (Espero que todo bien.)";
+            char [] separatorList = {'¡','¿','('};
+            string [] messageList = message.Split(separatorList);
+        }
+    }
+}
