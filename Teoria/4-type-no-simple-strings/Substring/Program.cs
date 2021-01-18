d432b413bbb45122bf9ad370f27131425b7ccf94
Miguel Angel Delgado <miguelexepg@gmail.com>
2021-01-18 10:11:43 +0100

    Substring

---
+++ Teoria/4-type-no-simple-strings/Substring/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "OneTwoThree";
+
+            // Vamos a usar para obtener tres numeros desde el texto
+
+            string resultOne = input.Substring(0,3);
+            string resultTwo = input.Substring(3,3);
+            string resultThree = input.Substring(6);
+
+            Console.WriteLine("Resultados: {0}-{1}-{2}", resultOne, resultTwo, resultThree);
+
+            // Vamos a obtener mediante letra capital => Onetwothree
+
+            string firstLetter = (input.Substring(0,1)).ToUpper();
+            string otherLetters = (input.Substring(1)).ToLower();
+            Console.WriteLine("Resultados para obtener con letra capital: \n{0}{1}", firstLetter, otherLetters);
+        }
+    }
+}
