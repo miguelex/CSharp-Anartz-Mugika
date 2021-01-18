826dfc7e1347e9a82423c2ad66bb4261dc396057
Miguel Angel Delgado <miguelexepg@gmail.com>
2021-01-18 10:39:25 +0100

    IndexOf

---
+++ Teoria/4-type-no-simple-strings/IndexOf/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IndexOf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "-> ¡Hola Anartz! Voy a aprender C# en Master Devs ";
+
+            int findAnartz = message.IndexOf("Anartz", 3); // 9 
+
+            findAnartz =  message.IndexOf("Anartz", 3); // 9
+            findAnartz =  message.IndexOf("Anartz", 15); // -1
+
+            int findMaster = message.IndexOf("Master Devs");
+
+            findMaster = message.IndexOf("Master Devs", 40);
+            findMaster = message.IndexOf("Master Devs", 38, 9);
+        }
+    }
+}
