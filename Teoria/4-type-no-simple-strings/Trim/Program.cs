22452180c112f5973a63679383c8d092c7656390
Miguel Angel Delgado <miguelexepg@gmail.com>
2021-01-18 11:41:56 +0100

    Trim

---
+++ Teoria/4-type-no-simple-strings/Trim/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Trim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "  ¡Soy Anartz!  ";
+            Console.WriteLine(text);
+            Console.WriteLine(text.TrimStart()+"=>");
+            Console.WriteLine(text.TrimEnd()+"=>");
+            Console.WriteLine(text.Trim()+"=>");
+        }
+    }
+}
