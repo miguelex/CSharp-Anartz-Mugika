ce87cf3d4952dda60b92b16a91c509f99ea2d70c
Miguel Angel Delgado <miguelexepg@gmail.com>
2021-01-18 11:10:20 +0100

    IndexOfSubstring

---
+++ Teoria/4-type-no-simple-strings/IndexOfSubstring/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IndexOfSubstring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "-> ¡Hola Anartz! Voy a a aprender C# en Master Devs (https://formacion.masterdevs.es/) ¡Animate!";
+            
+            int posInicial = message.IndexOf("Voy");
+            int posFinal = message.IndexOf(" ¡Animate!");
+
+            string subMessage = message.Substring(posInicial, posFinal-posInicial);
+            Console.WriteLine(subMessage);
+        }
+    }
+}
